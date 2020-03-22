08d59e67a41d186d6871b4311e17d54ba715b0c2
amaraSanoh <sanohawa@gmail.com>
2020-03-22 22:47:29 +0100

    Correctifs des formulaires et messages alert

--- src/main/java/com/projet/gestionJeux/controllers/EditeurJeuController.java
+++ src/main/java/com/projet/gestionJeux/controllers/EditeurJeuController.java
@@ -1,15 +1,19 @@
 package com.projet.gestionJeux.controllers;
 
 import com.projet.gestionJeux.models.EditeurJeu;
+import com.projet.gestionJeux.models.GenreJeu;
 import com.projet.gestionJeux.services.EditeurJeuService;
 import com.projet.gestionJeux.services.JeuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -33,6 +37,15 @@ public class EditeurJeuController {
 	public String getEditeurJeux(HttpServletRequest request, ModelMap model) {
 		List<EditeurJeu> editeurJeux = editeurJeuService.getEditeurJeux();
 		model.put("editeurs", editeurJeux);
+		
+		 Map<String, ?> flashMap = RequestContextUtils.getInputFlashMap(request); // 1
+		 if (!CollectionUtils.isEmpty(flashMap)) {
+	    	if("deleteSucess" == (String)flashMap.get("typeMessage")) {
+	    		model.put((String)flashMap.get("typeMessage"),((boolean) flashMap.get("typeMessagevalue"))); 
+	    	}
+	    	else model.put((String)flashMap.get("typeMessage"),editeurJeuService.existeEditeurJeu((int) flashMap.get("editorId"))); 
+		 }
+		    
 		return "Editeur/GestionEditeur";
 	}
 	
@@ -40,28 +53,22 @@ public class EditeurJeuController {
 	 * Créer un éditeur
 	 */
 	@PostMapping("/gestion-jeux/ajout-editeur")
-	public String creerOuModifierEditeur(HttpServletRequest request, ModelMap model) {
+	public String creerOuModifierEditeur(HttpServletRequest request, ModelMap model, RedirectAttributes redirectAttributes) {
 		String editeurNom = (String) request.getParameter("nom");
 		System.out.println(editeurNom);
 		EditeurJeu editeur = new EditeurJeu();
 		editeur.setNom_editeur(editeurNom);
-		EditeurJeu editeurJeu = this.editeurJeuService.saveOrUpdate(editeur);
-		
-		if (editeurJeuService.existeEditeurJeu(editeurJeu.getId())){
-			model.put("addSucess",true);
-		}
-		else{
-			model.put("addSucess",false);
-		}
-		
-		List<EditeurJeu> editeurs = editeurJeuService.getEditeurJeux();
-		model.put("editeurs", editeurs);
-		return "Editeur/GestionEditeur";
+		EditeurJeu editeurJeu = this.editeurJeuService.saveOrUpdate(editeur);	
+
+		redirectAttributes.addFlashAttribute("editorId", editeurJeu.getId()); 
+		redirectAttributes.addFlashAttribute("typeMessage", "addSucess"); 
+    
+    	return "redirect:/gestion-jeux/editeurs"; 
 		 
 	}
 	
 	@PostMapping("/gestion-jeux/edit-editeur")
-	public String editEditeur(HttpServletRequest request, ModelMap model) {
+	public String editEditeur(HttpServletRequest request, ModelMap model, RedirectAttributes redirectAttributes) {
 		String editeur_id = request.getParameter("editKind_id");
 		String editeurNom = (String) request.getParameter("nom");
 		System.out.println(editeurNom);
@@ -70,16 +77,10 @@ public class EditeurJeuController {
 		editeur.setNom_editeur(editeurNom); 
 		EditeurJeu editeurJeu = this.editeurJeuService.saveOrUpdate(editeur);
 		
-		if (editeurJeuService.existeEditeurJeu(editeurJeu.getId())){
-			model.put("updateSucess",true);
-		}
-		else{
-			model.put("updateSucess",false);
-		}
-		
-		List<EditeurJeu> editeurs = editeurJeuService.getEditeurJeux();
-		model.put("editeurs", editeurs);
-		return "Editeur/GestionEditeur";
+		redirectAttributes.addFlashAttribute("editorId", editeurJeu.getId()); 
+		redirectAttributes.addFlashAttribute("typeMessage", "updateSucess"); 
+    
+    	return "redirect:/gestion-jeux/editeurs"; 
 		 
 	}
 	
@@ -88,21 +89,22 @@ public class EditeurJeuController {
 	 * Un éditeur supprimé, supprimera tous les jeux associés à ce éditeur
 	 */
 	@GetMapping("/gestion-jeux/deleteEditeur/{id}")
-	public String supprimerEditeur(@PathVariable("id") int id, ModelMap model)
+	public String supprimerEditeur(@PathVariable("id") int id, ModelMap model, RedirectAttributes redirectAttributes)
 	{
-	  if (editeurJeuService.existeEditeurJeu(id)) {
-		  //Gérer d'abord la suppression des jeux associés
-		  //this.jeuService.deleteJeuxByIdEditeur(id);
-	      this.editeurJeuService.deleteEditeurJeu(id);
-	      model.put("deleteSucess",true);
-      }
-	  else{
-		  model.put("deleteSucess",false);
-	  }
 	  
-	  List<EditeurJeu> editeurs = editeurJeuService.getEditeurJeux();
-	  model.put("editeurs", editeurs);
-	  return "Editeur/GestionEditeur";
+	  	if (editeurJeuService.existeEditeurJeu(id)) {
+	  		this.editeurJeuService.deleteEditeurJeu(id);
+		  redirectAttributes.addFlashAttribute("typeMessagevalue", true); 
+	    }
+		else{
+	      model.put("deleteSucess",false);
+	      redirectAttributes.addFlashAttribute("typeMessagevalue", false); 
+	    }
+		
+		redirectAttributes.addFlashAttribute("editorId", id); 
+		redirectAttributes.addFlashAttribute("typeMessage", "deleteSucess"); 
+
+		return "redirect:/gestion-jeux/editeurs";
 	}
 	
 }
--- src/main/java/com/projet/gestionJeux/controllers/ThemeJeuController.java
+++ src/main/java/com/projet/gestionJeux/controllers/ThemeJeuController.java
@@ -1,6 +1,7 @@
 package com.projet.gestionJeux.controllers;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -8,9 +9,12 @@ import com.projet.gestionJeux.models.ThemeJeu;
 import com.projet.gestionJeux.services.ThemeJeuService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 
 @Controller
@@ -31,6 +35,15 @@ public class ThemeJeuController {
 	public String getThemeJeux(HttpServletRequest request, ModelMap model) {
 		List<ThemeJeu> themeJeux = themeJeuService.getThemeJeux();
 		model.put("themes", themeJeux);
+		
+		 Map<String, ?> flashMap = RequestContextUtils.getInputFlashMap(request); // 1
+	    if (!CollectionUtils.isEmpty(flashMap)) {
+	    	if("deleteSucess" == (String)flashMap.get("typeMessage")) {
+	    		model.put((String)flashMap.get("typeMessage"),((boolean) flashMap.get("typeMessagevalue"))); 
+	    	}
+	    	else model.put((String)flashMap.get("typeMessage"),themeJeuService.existeThemeJeu((int) flashMap.get("themeId"))); 
+	    }
+	    
 		return "Theme/GestionTheme";
 	}
 	
@@ -38,28 +51,22 @@ public class ThemeJeuController {
 	 * Créer un thème
 	 */
 	@PostMapping("/gestion-jeux/ajout-theme")
-	public String creerOuModifierTheme(HttpServletRequest request, ModelMap model) {
+	public String creerOuModifierTheme(HttpServletRequest request, ModelMap model, RedirectAttributes redirectAttributes) {
 		String themeNom = (String) request.getParameter("nom");
 		System.out.println(themeNom);
 		ThemeJeu theme = new ThemeJeu();
 		theme.setNom_theme(themeNom);
 		ThemeJeu themeJeu = this.themeJeuService.saveOrUpdate(theme);
 		
-		if (themeJeuService.existeThemeJeu(themeJeu.getId())){
-			model.put("addSucess",true);
-		}
-		else{
-			model.put("addSucess",false);
-		}
-		
-		List<ThemeJeu> themes = themeJeuService.getThemeJeux();
-		model.put("themes", themes);
-		return "Theme/GestionTheme";
+		redirectAttributes.addFlashAttribute("themeId", themeJeu.getId()); 
+		redirectAttributes.addFlashAttribute("typeMessage", "addSucess"); 
+    
+    	return "redirect:/gestion-jeux/themes"; 
 		 
 	}
 	
 	@PostMapping("/gestion-jeux/edit-theme")
-	public String editTheme(HttpServletRequest request, ModelMap model) {
+	public String editTheme(HttpServletRequest request, ModelMap model, RedirectAttributes redirectAttributes) {
 		String theme_id = request.getParameter("editKind_id");
 		String themeNom = (String) request.getParameter("nom");
 		System.out.println(themeNom);
@@ -68,17 +75,10 @@ public class ThemeJeuController {
 		theme.setNom_theme(themeNom); 
 		ThemeJeu themeJeu = this.themeJeuService.saveOrUpdate(theme);
 		
-		if (themeJeuService.existeThemeJeu(themeJeu.getId())){
-			model.put("updateSucess",true);
-		}
-		else{
-			model.put("updateSucess",false);
-		}
-		
-		
-		List<ThemeJeu> themes = themeJeuService.getThemeJeux();
-		model.put("themes", themes);
-		return "Theme/GestionTheme";
+		redirectAttributes.addFlashAttribute("themeId", themeJeu.getId()); 
+		redirectAttributes.addFlashAttribute("typeMessage", "updateSucess"); 
+    
+    	return "redirect:/gestion-jeux/themes"; 
 		 
 	}
 	
@@ -86,18 +86,21 @@ public class ThemeJeuController {
 	 * Supprimer un thème
 	 */
 	@GetMapping("/gestion-jeux/deleteTheme/{id}")
-	public String supprimerTheme(@PathVariable("id") int id, ModelMap model)
+	public String supprimerTheme(@PathVariable("id") int id, ModelMap model, RedirectAttributes redirectAttributes)
 	{
-	  if (themeJeuService.existeThemeJeu(id)) {
-	      this.themeJeuService.deleteThemeJeu(id);
-	      model.put("deleteSucess",true);
-      }
-	  else{
-		  model.put("deleteSucess",false);
-	  }
-	  
-	  List<ThemeJeu> themes = themeJeuService.getThemeJeux();
-	  model.put("themes", themes);
-	  return "Theme/GestionTheme";
+		
+	  	if (themeJeuService.existeThemeJeu(id)) {
+	  		this.themeJeuService.deleteThemeJeu(id);
+		  redirectAttributes.addFlashAttribute("typeMessagevalue", true); 
+	    }
+		else{
+	      model.put("deleteSucess",false);
+	      redirectAttributes.addFlashAttribute("typeMessagevalue", false); 
+	    }
+		
+		redirectAttributes.addFlashAttribute("themeId", id); 
+		redirectAttributes.addFlashAttribute("typeMessage", "deleteSucess"); 
+	
+		return "redirect:/gestion-jeux/themes";
 	}
 }
