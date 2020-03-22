e2d68d72e8bace68718af22d897f8d72bf0dc3db
amaraSanoh <sanohawa@gmail.com>
2020-03-22 22:04:56 +0100

    Ressoumission du formulaire, modale pour la suppresssion de genre

--- src/main/java/com/projet/gestionJeux/controllers/GenreJeuController.java
+++ src/main/java/com/projet/gestionJeux/controllers/GenreJeuController.java
@@ -1,6 +1,7 @@
 package com.projet.gestionJeux.controllers;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -9,11 +10,12 @@ import com.projet.gestionJeux.services.GenreJeuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 @Controller
 public class GenreJeuController {
@@ -27,10 +29,19 @@ public class GenreJeuController {
 	 * @return List<GenreJeu>
 	 */
 	@GetMapping("/gestion-jeux/genres")
-	public String getGenreJeux( ModelMap model) {
-			List<GenreJeu> genres = genreJeuService.getGenreJeux();
-			model.put("genres", genres);
-			return "Genre/GestionGenre";
+	public String getGenreJeux( ModelMap model, HttpServletRequest request) {
+		List<GenreJeu> genres = genreJeuService.getGenreJeux();
+		model.put("genres", genres);
+		
+	    Map<String, ?> flashMap = RequestContextUtils.getInputFlashMap(request); // 1
+	    if (!CollectionUtils.isEmpty(flashMap)) {
+	    	if("deleteSucess" == (String)flashMap.get("typeMessage")) {
+	    		model.put((String)flashMap.get("typeMessage"),((boolean) flashMap.get("typeMessagevalue"))); 
+	    	}
+	    	else model.put((String)flashMap.get("typeMessage"),genreJeuService.existeGenreJei((int) flashMap.get("kindId"))); 
+	    }
+
+	    return "Genre/GestionGenre";
 	}
 	
 	/**
@@ -38,29 +49,22 @@ public class GenreJeuController {
 	 * Créer un genre
 	 */
 	@PostMapping("/gestion-jeux/ajout-genre")
-	public String creerOuModifierGenre(HttpServletRequest request, ModelMap model) {
+	public String creerOuModifierGenre(HttpServletRequest request, ModelMap model, RedirectAttributes redirectAttributes) {
 		String genreNom = (String) request.getParameter("nom");
 		GenreJeu genre = new GenreJeu();
 		genre.setNom_genre(genreNom);
 
 		//rajout et recupere le genre du jeu inséré
 		GenreJeu genreJeu = this.genreJeuService.saveOrUpdate(genre);
-
-    if (genreJeuService.existeGenreJei(genreJeu.getId())){
-      model.put("addSucess",true);
-    }
-    else{
-      model.put("addSucess",false);
-    }
-		
-		List<GenreJeu> genres = genreJeuService.getGenreJeux();
-		model.put("genres", genres);
-		return "Genre/GestionGenre";
+		redirectAttributes.addFlashAttribute("kindId", genre.getId()); 
+		redirectAttributes.addFlashAttribute("typeMessage", "addSucess"); 
+    
+    	return "redirect:/gestion-jeux/genres"; 
 		 
 	}
 	
 	@PostMapping("/gestion-jeux/edit-genre")
-	public String editGenre(HttpServletRequest request, ModelMap model) {
+	public String editGenre(HttpServletRequest request, ModelMap model, RedirectAttributes redirectAttributes) {
 		String genre_id = request.getParameter("editKind_id");
 		String genreNom = (String) request.getParameter("nom");
 		System.out.println(genreNom);
@@ -68,20 +72,11 @@ public class GenreJeuController {
 		GenreJeu genre = this.genreJeuService.findById(genreId);
 		genre.setNom_genre(genreNom); 
 
-    GenreJeu genreJeu = this.genreJeuService.saveOrUpdate(genre);
-
-    if (genreJeuService.existeGenreJei(genreJeu.getId())){
-      model.put("updateSucess",true);
-    }
-    else{
-      model.put("updateSucess",false);
-    }
-
-
-		List<GenreJeu> genres = genreJeuService.getGenreJeux();
-		model.put("genres", genres);
-		return "Genre/GestionGenre";
-		 
+		GenreJeu genreJeu = this.genreJeuService.saveOrUpdate(genre);
+		redirectAttributes.addFlashAttribute("kindId", genre.getId()); 
+		redirectAttributes.addFlashAttribute("typeMessage", "updateSucess"); 
+    	
+    	return "redirect:/gestion-jeux/genres"; 
 	}
 	
 	
@@ -89,20 +84,21 @@ public class GenreJeuController {
 	 * Supprimer un genre
 	 */
 	@GetMapping("/gestion-jeux/deleteGenre/{id}")
-	public String supprimerGenre(@PathVariable("id") int id, ModelMap model)
+	public String supprimerGenre(@PathVariable("id") int id, ModelMap model, RedirectAttributes redirectAttributes)
 	{
-	  if (genreJeuService.existeGenreJei(id)) {
-      this.genreJeuService.deleteGenreJeu(id);
-      model.put("deleteSucess",true);
-    }
-	  else{
-      model.put("deleteSucess",false);
+		if (genreJeuService.existeGenreJei(id)) {
+	      this.genreJeuService.deleteGenreJeu(id);
+		  redirectAttributes.addFlashAttribute("typeMessagevalue", true); 
+	    }
+		else{
+	      model.put("deleteSucess",false);
+	      redirectAttributes.addFlashAttribute("typeMessagevalue", false); 
+	    }
+		
+		redirectAttributes.addFlashAttribute("kindId", id); 
+		redirectAttributes.addFlashAttribute("typeMessage", "deleteSucess"); 
 
-    }
-
-		List<GenreJeu> genres = genreJeuService.getGenreJeux();
-		model.put("genres", genres);
-    return "Genre/GestionGenre";
+		return "redirect:/gestion-jeux/genres";
 	}
 
 }
